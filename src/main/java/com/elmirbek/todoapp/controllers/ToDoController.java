46a8e66316ef4ccf5e38c7eeaefc2058afbc443c
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-11 16:56:55 +0600

    add task creation feature

---
+++ src/main/java/com/elmirbek/todoapp/controllers/ToDoController.java
@@ -0,0 +1,14 @@
+package com.elmirbek.todoapp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ToDoController {
+
+    @GetMapping
+   public String index () {
+        return "index";
+    }
+}
