2f78c04932466363992c4e28eec8c5a4bcb7b9ec
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-12 22:23:13 +0600

    Add TodoItem entity with id and title fields for task storage

--- src/main/java/com/elmirbek/todoapp/model/TodoItem.java
+++ src/main/java/com/elmirbek/todoapp/model/TodoItem.java
@@ -8,6 +8,29 @@ public class TodoItem {
 
     @Id
     @GeneratedValue
-    Long id;
-    String title;
+    private Long id;
+    private String title;
+
+    public TodoItem(String title) {
+        this.title = title;
+    }
+
+    public TodoItem() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
