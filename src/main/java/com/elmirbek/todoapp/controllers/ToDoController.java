2733376cc2b681b54e23aa26aad3e494554d1be3
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-12 22:22:37 +0600

    Add ToDoController with GET mapping to display all todo items and prepopulate with sample data

--- src/main/java/com/elmirbek/todoapp/controllers/ToDoController.java
+++ src/main/java/com/elmirbek/todoapp/controllers/ToDoController.java
@@ -1,14 +1,31 @@
 package com.elmirbek.todoapp.controllers;
+import com.elmirbek.todoapp.model.TodoItem;
+import com.elmirbek.todoapp.repositories.TodoItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ToDoController {
 
+    private final TodoItemRepository todoItemRepository;
+
+    public ToDoController(TodoItemRepository todoItemRepository) {
+        this.todoItemRepository = todoItemRepository;
+    }
+
     @GetMapping
    public String index (Model model) {
-        model.addAttribute("data", "Hello");
+        todoItemRepository.save(new TodoItem("Item 1"));
+        todoItemRepository.save(new TodoItem("Item 2"));
+        todoItemRepository.save(new TodoItem("Item 3"));
+
+      List<TodoItem> allTodos = todoItemRepository.findAll();
+      model.addAttribute("allTodos", allTodos);
+
+
         return "index";
     }
 }
