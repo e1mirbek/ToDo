7993d2a793189006eb16feff0f1eb7dad127e299
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-13 15:35:23 +0600

    feat: добавлен контроллер ToDo со страницей отображения, добавлением и удалением задач

--- src/main/java/com/elmirbek/todoapp/controllers/ToDoController.java
+++ src/main/java/com/elmirbek/todoapp/controllers/ToDoController.java
@@ -1,14 +1,18 @@
 package com.elmirbek.todoapp.controllers;
 import com.elmirbek.todoapp.model.TodoItem;
 import com.elmirbek.todoapp.repositories.TodoItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
-public class ToDoController {
+public class ToDoController implements CommandLineRunner {
 
     private final TodoItemRepository todoItemRepository;
 
@@ -18,14 +22,36 @@ public class ToDoController {
 
     @GetMapping
    public String index (Model model) {
-        todoItemRepository.save(new TodoItem("Item 1"));
-        todoItemRepository.save(new TodoItem("Item 2"));
-        todoItemRepository.save(new TodoItem("Item 3"));
-
       List<TodoItem> allTodos = todoItemRepository.findAll();
       model.addAttribute("allTodos", allTodos);
+      model.addAttribute("newTodo", new TodoItem());
 
 
         return "index";
+    }
+
+
+    // добавление задачи
+    @PostMapping("/add")
+    public String add(@ModelAttribute  TodoItem todoItem){
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
+
+    // удаление задачи
+    @PostMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") Long id) {
+        todoItemRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        todoItemRepository.save(new TodoItem("Задача 1"));
+        todoItemRepository.save(new TodoItem("Задача 2"));
+        todoItemRepository.save(new TodoItem("Задача 3"));
     }
 }
